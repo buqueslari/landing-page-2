6a39ac983d146b576d00fad9ff060e4a96986aa9
buqueslari <buqueslari@gmail.com>
2025-06-04 00:03:38 -0300

    update number

--- components/common/ScrollTop.jsx
+++ components/common/ScrollTop.jsx
@@ -7,7 +7,7 @@ export default function ScrollTop({ hasPaddingBottom = false }) {
   const [isButtonVisible] = useState(true);
 
   const handleScrollToTop = () => {
-    const telefone = "551151929639";
+    const telefone = "5511954946002";
     let mensagem = "Olá! Estou interessado(a) nos produtos da sua loja. Poderia me dar mais informações? Obrigado!";
 
     const mensagemCodificada = encodeURIComponent(mensagem);
--- components/otherPages/ShopCart.jsx
+++ components/otherPages/ShopCart.jsx
@@ -46,7 +46,7 @@ export default function ShopCart() {
       return;
     }
 
-    const telefone = "551151929639";
+    const telefone = "5511954946002";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
