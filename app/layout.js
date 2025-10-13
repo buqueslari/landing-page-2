f22cb665862e5f6988d07d3b6cd03e5c548b6e4a
buqueslari <buqueslari@gmail.com>
2025-10-13 03:05:17 -0300

    update tag teste

--- app/layout.js
+++ app/layout.js
@@ -125,14 +125,14 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager Script */}
         <Script
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17545551723"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17545551001"
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-17545551723');
+            gtag('config', 'AW-17545551001');
           `}
         </Script>
       </head>
