c376386dd229220588225015b6f2bf7483ce03c6
buqueslari <buqueslari@gmail.com>
2025-11-03 23:13:06 -0300

    update tag goole

--- app/layout.js
+++ app/layout.js
@@ -125,14 +125,14 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager Script */}
         <Script
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17665811401"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17695795967"
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-17665811401');
+            gtag('config', 'AW-17695795967');
           `}
         </Script>
       </head>
