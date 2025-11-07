34b445f1f6ede74e56a08711c53ba2f460578eb9
buqueslari <buqueslari@gmail.com>
2025-11-07 08:53:28 -0300

    update number

--- components/otherPages/ShopCart.jsx
+++ components/otherPages/ShopCart.jsx
@@ -46,7 +46,7 @@ export default function ShopCart() {
       return;
     }
 
-    const telefone = "5511986373737";
+    const telefone = "5511965799517";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
