6f18a5dc81926c901a2a90982067527f481a9864
buqueslari <buqueslari@gmail.com>
2025-11-30 07:07:04 -0300

    uptr number

--- components/otherPages/ShopCart.jsx
+++ components/otherPages/ShopCart.jsx
@@ -46,7 +46,7 @@ export default function ShopCart() {
       return;
     }
 
-    const telefone = "551151521917";
+    const telefone = "85996794744";
     let mensagem = "Olá, gostaria desses produtos:\n\n";
     let total = 0;
 
