93cae48da981bdb93f53b8c3f49ed55eb5b4340a
buqueslari <buqueslari@gmail.com>
2026-01-28 10:59:17 -0300

    update tag

--- app/layout.js
+++ app/layout.js
@@ -125,14 +125,14 @@ export default function RootLayout({ children }) {
         {/* Google Tag Manager Script */}
         <Script
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17851975067"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17899645764"
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'AW-17851975067');
+            gtag('config', 'AW-17899645764');
           `}
         </Script>
       </head>
